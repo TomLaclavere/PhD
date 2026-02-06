39241e38d799e0a992747ecc6e504a86d83a6149
TomLaclavere <laclavere@apc.in2p3.fr>
2026-02-06 15:09:21 +0100

    test

--- scripts/website/local_build_website.sh
+++ scripts/website/local_build_website.sh
@@ -11,5 +11,11 @@ export OUTPUT_DIR="Thesis/output"
 export CHAPTER_DIR="$OUTPUT_DIR/chapters"
 export CURRENT_DATE="$(date +"%Y-%m-%d")"
 
+# Compile thesis
+./scripts/website/local_gen_thesis.sh
+
+# Compiles chapter
+./scripts/website/local_gen_chapters.sh
+
 # Generate website
 ./scripts/website/build_website.sh
