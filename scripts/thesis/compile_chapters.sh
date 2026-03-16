4eca05ef245a50110c6c8aaf882c255e0c9b536b
TomLaclavere <tomlaclavere@gmail.com>
2026-03-16 14:19:23 +0000

    solve problem with table of contents + chapter 1

--- scripts/thesis/compile_chapters.sh
+++ scripts/thesis/compile_chapters.sh
@@ -25,7 +25,7 @@ for chapter_dir in "$CHAPTERS_DIR"/*/; do
 
   # Trap for compilation errors
   function on_error {
-      echo "Chapter compilation failed! Please check logs in $output_dir/$tex_basename"
+      echo "Chapter compilation failed! Please check logs in $chapter_dir/$tex_basename"
   }
   trap on_error ERR
 
